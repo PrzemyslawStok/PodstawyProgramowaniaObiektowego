965f1afffa46d27e40e15f719ae72f1ffc6f09ca
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2022-10-07 18:46:43 +0200

    cd ..

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from student import Student
+import numpy as np
 
 if __name__ == '__main__':
     print("Programowanie obiektowe")
 
+    student1 = Student()
 
-
+    student1.print()
--- student.py
+++ student.py
@@ -1,2 +1,11 @@
+import numpy as np
+
+
 class Student():
-    pass
+    def __init__(self):
+        self.name = "Przemysław"
+        self.surname = "Stokłosa"
+        self.id = np.random.randint(0, 1000)
+
+    def print(self):
+        print(f"Imię: {self.name} Nazwisko: {self.surname}, id: {self.id}")
