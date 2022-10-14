1fd64d3079862fcadc07f05c39ab33650fcf4e2d
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2022-10-14 18:33:20 +0200

    cd ..

--- line.py
+++ line.py
@@ -1,3 +1,9 @@
 class Line:
-    def __init__(self):
-        return 0
+    def __init__(self, length: float):
+        self.length = length
+
+    def __add__(self, other):
+        return Line(self.length + other.length)
+
+    def print(self):
+        print(f"Długość lini wynosi: {self.length}")
