20b29aeca0e633e31c6ba633a5c91d5adb6bf133
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2022-10-14 18:13:59 +0200

    cd ..

--- square.py
+++ square.py
@@ -1,13 +1,13 @@
 class Square():
     def __init__(self, size: float):
-        self.size = size
+        self.a = size
 
     def print(self):
-        print(f"kwadrat o boku: {self.size}")
+        print(f"kwadrat o boku: {self.a}, powierzchnia: {self.area()}")
 
     def area(self) -> float:
-        return self.size * self.size
+        return self.a * self.a
 
     def __add__(self, other):
-        newSize = self.size + other.size
+        newSize = self.a + other.a
         return Square(newSize)
