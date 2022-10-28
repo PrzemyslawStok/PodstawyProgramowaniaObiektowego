93be2106ce2febbdd2193b054f918c029ddda87e
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2022-10-28 19:08:29 +0200

    cd ..

--- University/main.py
+++ University/main.py
@@ -1,4 +1,5 @@
 from University.university_obj import University
 
 if __name__ == "__main__":
-    university = University()
+    university = University(university_name="WSIZ", no_students=10, no_teachers=2)
+    university.print()
--- University/university_obj.py
+++ University/university_obj.py
@@ -1,3 +1,25 @@
+from University.student import Student
+
+
 class University:
-    def __init__(self):
+    def __init__(self, university_name, no_teachers, no_students):
+        self.university_name = university_name
+        self.no_students = no_students
+        self.no_teachers = no_teachers
+
+        self.students_list = []
+        self.teachers_list = []
+
+        self.add_students()
+        self.add_teachers()
+
+    def add_students(self):
+        for i in range(self.no_students):
+            self.students_list.append(Student("Piotr", f"Stokłosa_{i}"))
+
+    def add_teachers(self):
         pass
+
+    def print(self):
+        print(f"Nazwa uczelni: {self.university_name}")
+        print("Lista nauczycieli: ")
