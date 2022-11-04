49d51e5a6a522697c96802ad36441f918fec3b67
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2022-11-04 18:17:33 +0100

    cd ..

--- Figures2D/main.py
+++ Figures2D/main.py
@@ -1,11 +1,13 @@
 from Figures2D.circle import Circle
+from Figures2D.figure2D import Figure2D
 from Figures2D.square import Square
 
 if __name__ == "__main__":
     square = Square(a=10)
     circle = Circle()
 
-    figures_list = []
+    figures_list:list[Figure2D] = []
+
     figures_list.append(square)
     figures_list.append(circle)
 
