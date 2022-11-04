acf0eb169350422649ae1613672ca63c6539f28a
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2022-11-04 18:56:07 +0100

    cd ..

--- Figures2D/triangle.py
+++ Figures2D/triangle.py
@@ -1,4 +1,11 @@
 from Figures2D.figure2D import Figure2D
+import numpy as np
+
 
 class Triangle(Figure2D):
-    pass
+    def __init__(self, a: float = None):
+        super().__init__(name="trójkąt")
+        self.a = a
+
+    def area(self):
+        return np.sqrt(3) / 4.0 * self.a * self.a
