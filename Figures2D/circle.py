0446d58c4a47a490682d1b63ea9f7e19db89f05f
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2022-11-04 18:44:42 +0100

    cd ..

--- Figures2D/circle.py
+++ Figures2D/circle.py
@@ -1,6 +1,11 @@
 from Figures2D.figure2D import Figure2D
+import numpy as np
 
 
 class Circle(Figure2D):
     def __init__(self, r: float = None):
-        super().__init__()
+        super().__init__(name="koło")
+        self.r = r
+
+    def area(self):
+        return np.pi * self.r * self.r
---
+++ Figures2D/triangle.py
@@ -0,0 +1,4 @@
+from Figures2D.figure2D import Figure2D
+
+class Triangle(Figure2D):
+    pass
