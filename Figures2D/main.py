c88717f69f63be90cee929c4992b542fcf1b3f8b
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2022-11-18 19:03:22 +0100

    cd ..

--- Figures2D/main.py
+++ Figures2D/main.py
@@ -7,7 +7,7 @@ from Figures2D.triangle import Triangle
 def addArea(figures_list: list[Figure2D]) -> float:
     areaSum = 0
     for figure in figures_list:
-        area = figure.area()
+        areaSum += figure.area()
 
     return areaSum
 
