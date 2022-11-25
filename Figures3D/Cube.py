9d89e27e34575ee4051f991d4ed5fef4df2ed3c3
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2022-11-25 18:57:29 +0100

    cd ..

--- Figures3D/Cube.py
+++ Figures3D/Cube.py
@@ -1,4 +1,9 @@
+from Figures2D.figure2D import Figure2D
 from Figures3D.Prism import Prism
 
+
 class Cube(Prism):
-    pass
+    def __init__(self, a: float):
+        self.name = "Cube"
+        self.a = a
+        super().__init__(base, h=a, name=self.name)
